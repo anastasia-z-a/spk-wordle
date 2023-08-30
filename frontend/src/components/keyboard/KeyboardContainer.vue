f554951eaae4d8afb800009090b0d5aef0809fee
adrian.nysted.riise@spk.no <adrian.nysted.riise@spk.no>
2023-08-30 10:57:38 +0200

    initial frontend setup

---
+++ frontend/src/components/keyboard/KeyboardContainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    style="display: flex; flex-direction: column; place-items: center; gap: 4px"
+  >
+    <div style="display: flex; flex-direction: row; gap: 4px">
+      <q-btn outline round color="blue" label="Q" />
+      <q-btn outline round color="blue" label="W" />
+      <q-btn outline round color="blue" label="E" />
+      <q-btn outline round color="blue" label="R" />
+      <q-btn outline round color="blue" label="T" />
+      <q-btn outline round color="blue" label="Y" />
+      <q-btn outline round color="blue" label="U" />
+      <q-btn outline round color="blue" label="I" />
+      <q-btn outline round color="blue" label="O" />
+      <q-btn outline round color="blue" label="P" />
+      <q-btn outline round color="blue" label="Å" />
+    </div>
+    <div style="display: flex; flex-direction: row; gap: 4px">
+      <q-btn outline round color="blue" label="A" />
+      <q-btn outline round color="blue" label="S" />
+      <q-btn outline round color="blue" label="D" />
+      <q-btn outline round color="blue" label="F" />
+      <q-btn outline round color="blue" label="G" />
+      <q-btn outline round color="blue" label="H" />
+      <q-btn outline round color="blue" label="J" />
+      <q-btn outline round color="blue" label="K" />
+      <q-btn outline round color="blue" label="L" />
+      <q-btn outline round color="blue" label="Ø" />
+      <q-btn outline round color="blue" label="Æ" />
+    </div>
+    <div style="display: flex; flex-direction: row; gap: 4px">
+      <q-btn outline round color="blue" label="Z" />
+      <q-btn outline round color="blue" label="X" />
+      <q-btn outline round color="blue" label="C" />
+      <q-btn outline round color="blue" label="V" />
+      <q-btn outline round color="blue" label="B" />
+      <q-btn outline round color="blue" label="N" />
+      <q-btn outline round color="blue" label="M" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-black window-height window-width">
+    <KeyboardContainer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import KeyboardContainer from 'src/components/keyboard/KeyboardContainer.vue';
+</script>
